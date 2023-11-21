a3b1208fb9ffcff9623c100829f3718968fe9996
xavi <xavi.hidalgo.fernandez@gmail.com>
2023-11-21 18:47:02 +0100

    run test

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,9 +1,13 @@
 set -i
+
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Darwin*)    EXTRA_FLAGS="-I/opt/homebrew/include -L/opt/homebrew/lib";;
     *)          EXTRA_FLAGS=""
 esac
 
-gcc -o test first_test.c  ${EXTRA_FLAGS} -lcriterion
+TEST_NAME="first_test.c"
+
+gcc -o test ${TEST_NAME} ${EXTRA_FLAGS} -lcriterion
 ./test
