a857aaeb0aef736af1858f65d7812fc05c340596
xavi <xavi.hidalgo.fernandez@gmail.com>
2023-11-21 18:58:02 +0100

    fix test

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,11 +1,14 @@
 set -i
+
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Darwin*)    EXTRA_FLAGS="-I/opt/homebrew/include -L/opt/homebrew/lib";;
     *)          EXTRA_FLAGS=""
 esac
 
-TEST_NAME="first_test.c"
+TEST_NAME="string_parse_test.c"
 
 gcc -o test ${TEST_NAME} ${EXTRA_FLAGS} -lcriterion
 ./test
+
