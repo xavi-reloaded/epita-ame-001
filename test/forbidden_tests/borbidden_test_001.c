a342476b12f691b408c2a8f529ffb9795975b07a
xavi <xavi.hidalgo.fernandez@gmail.com>
2023-11-25 10:00:38 +0100

    forbidden test

---
+++ test/forbidden_tests/borbidden_test_001.c
@@ -0,0 +1,13 @@
+#include <criterion/criterion.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+Test(file, forbidden)
+{
+
+}
+
---
+++ test/forbidden_tests/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NC='\033[0m' # No Color
+IGreen='\033[0;92m'
+BICyan='\033[1;96m' 
+BIRed='\033[1;91m'
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Darwin*)    EXTRA_FLAGS="-I/opt/homebrew/include -L/opt/homebrew/lib";;
+    *)          EXTRA_FLAGS=""
+esac
+
+for f in ./*.c
+do
+    chmod -r ../test_files/access_forbidden.txt # write desired permissions
+	echo "Processing $f"
+    echo ""
+    echo -e "${BIRed}=== (${IGreen}1${BIRed}) Forbbiden Tests === [ ${BICyan} ${f} ${BIRed} ] ${NC} "
+    gcc -o test ${f} ${EXTRA_FLAGS} -lcriterion
+    ./test
+    echo -e "${BIRed}===================================================================${NC} "
+    echo ""
+    rm test
+    chmod -r ../test_files/access_forbidden.txt # restore desired permissions
+done
+
