16a7e1c0ed73150636783f31f52b71161e0fbcd2
Amélie Hidalgo Peryllous <amelie.hidalgo-peryllous@epita.fr>
2023-11-25 11:50:23 +0100

    wip: rules grande test

---
+++ test/rules2_test.c
@@ -0,0 +1,7 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../src/http/http.h"
+#include "../src/config/config.h"
+#include "../src/utils/string/string.h"
+
